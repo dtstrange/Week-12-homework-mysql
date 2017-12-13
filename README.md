c743f104dfc19b13941241c1ecf6ea87c0635518
Dustin Frank <dustintstrange@gmail.com>
2017-12-13 12:20:48 -0800

    completed bamazonCustomer.js and updated readme with links to screen shots

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Week-12-homework-mysql
+
+**bamazonCustomer.js screenshots**
+*Database before app use*
+https://drive.google.com/file/d/1aDHMl2hwXNDoBLkyJB5GG9DY57Ml8M8U/view?usp=sharing
+
+*bamazonCustomer use*
+https://drive.google.com/file/d/1jCSXmXybdKzEDA_ciOGh1tdU6LcM1N9f/view?usp=sharing
+
+*Database after app use*
+https://drive.google.com/file/d/1f8FqI2SLF_Bz5hpBtDwEW9n7r8sVnMZC/view?usp=sharing
+
+*bamazonCustomer insufficient stock*
+https://drive.google.com/open?id=1skRcGOTjcM4vS8QFTlmddrZlmoFnXu8v
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,6 @@
 //requirements
 var inquirer = require('inquirer');
-var mysql      = require('mysql');
+var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: 'localhost',
   port: '3306',
@@ -9,28 +9,50 @@ var connection = mysql.createConnection({
   database: 'bamazon'
 });
 
-connection.connect();
+function start() {
+  connection.connect();
 
+  //displaying table from database
+  connection.query('SELECT * FROM products', function (error, results, fields) {
+    if (error) throw error;
+    console.log(results);
+    afterConnection();
+  });
 
-function start (){
-    //displaying table from database
-    connection.query('SELECT * FROM products', function (error, results, fields) {
-        if (error) throw error;
-        console.log(results);
-      });
+  function afterConnection() {
 
-      inquirer.prompt([
-          {
-          type: 'input',
-          name: 'ID',
-          message: 'What is the product_id of the item you would like to purchase?'
-        },
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'ID',
+        message: 'What is the product_id of the item you would like to purchase?'
+      },
       {
         type: 'input',
         name: 'quantity',
         message: 'How many would you like to purchase?'
       }
-    ]).then()
-};
+    ]).then(function (answers) {
+      connection.query('SELECT * FROM products WHERE item_id = ?', [answers.ID], function (error, results, fields) {
+        if (error) throw error; 
+        
+        if (results[0].stock_quantity < answers.quantity) {
+          console.log("Insufficient quantity!");
+          connection.end();
+        }
+        else {
+          var newQty = Math.floor(Number(results[0].stock_quantity) - Number(answers.quantity));
+          connection.query('UPDATE products SET stock_quantity = ? WHERE item_id = ?', [newQty, answers.ID], function (error, newResults, fields) {
+            if (error) throw error;
+            console.log("Cost of your purchase: $" + Math.round(Number(answers.quantity) * Number(results[0].price)));
+            
+        
+            connection.end();
+          })
+        }
 
-start();
+      });
+    });
+  }
+}
+  start();
