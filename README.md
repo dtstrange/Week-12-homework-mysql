d8d1a07039d90934403cb7ddd2512c0bc5964ffc
Dustin Frank <dustintstrange@gmail.com>
2017-12-14 14:12:41 -0800

    completed bamazonManager.js updated readme

--- README.md
+++ README.md
@@ -12,3 +12,25 @@ https://drive.google.com/file/d/1f8FqI2SLF_Bz5hpBtDwEW9n7r8sVnMZC/view?usp=shari
 
 *bamazonCustomer insufficient stock*
 https://drive.google.com/open?id=1skRcGOTjcM4vS8QFTlmddrZlmoFnXu8v
+
+**bamazonManager.js screenshots**
+*view inventory*
+https://drive.google.com/open?id=1Cdsg9juFkSXrZqfH5uBD_q4eRUXmSP0C
+
+*view low inventory*
+https://drive.google.com/open?id=1OEMwi3fFVj9p8ZFmzxHQeLfDyxaQnvMs
+
+*bamazon db pre add to inventory*
+https://drive.google.com/open?id=1wk6F5Z-97Mbs5hUfpEji3sdpWNLLJaLQ
+
+*add to inventory*
+https://drive.google.com/open?id=1MbxeCJBKKnY1CUSm6V5kV-WZi6jrf7yy
+
+*db post add to inventory*
+https://drive.google.com/open?id=1QGtn6ux0xMtB95hCrUk8G1f2LmtpbCxo
+
+*add new product*
+https://drive.google.com/open?id=1hmyyA8b2-lHw1TAlJOq-0PNx8efXPrN0
+
+*db post add new product*
+https://drive.google.com/open?id=1eyQAjXC0jOxHdw6_qvn0jnObWcemCCMa
--- bamazonManager.js
+++ bamazonManager.js
@@ -9,9 +9,10 @@ var connection = mysql.createConnection({
     database: 'bamazon'
 });
 
-connection.connect();
+
 
 function openingMessage() {
+    connection.connect();
     inquirer.prompt([
         {
             type: 'list',
@@ -22,38 +23,114 @@ function openingMessage() {
                 'Exit'
             ]
         }
-   
-   
-    ])
-    .then(function (answers){
-        if(answers.start === 'Start'){
-            start();
-        }else{
-            connection.end();
-        }
-    })
 
-    
+
+    ])
+        .then(function (answers) {
+            if (answers.start === 'Start') {
+                start();
+            } else {
+                connection.end();
+            }
+        })
+
+
 };
 
-    function start(){
-        inquirer.prompt([
-            {
-                type: 'list',
-                name: 'inventory',
-                message: 'What would you like to do?',
-                choices: [
-                    'View Products for Sale',
-                    'View Low Inventory',
-                    'Add to Inventory',
-                    'Add New Product'
-                ]
+function start() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'inventory',
+            message: 'What would you like to do?',
+            choices: [
+                'View Products for Sale',
+                'View Low Inventory',
+                'Add to Inventory',
+                'Add New Product',
+                'Exit'
+            ]
+        }
+    ])
+        .then(function (answers) {
+            if (answers.inventory === 'View Products for Sale') {
+                connection.query('SELECT * FROM products', function (error, results, fields) {
+                    if (error) throw error;
+                    console.log(JSON.stringify(results, null, 2));
+                    start();
+                })
+            } else if (answers.inventory === 'View Low Inventory') {
+                connection.query('SELECT * FROM products WHERE stock_quantity < 5', function (error, lowResults, fields) {
+                    if (error) throw error;
+                    console.log(JSON.stringify(lowResults, null, 2));
+                    start();
+                })
+            } else if (answers.inventory === 'Add to Inventory') {
+                addInventory();
+            } else if (answers.inventory === 'Add New Product') {
+                addProduct();
+            } else if (answers.inventory === 'Exit') {
+                connection.end();
             }
-        ])
-        .then(function (answers){
-            
         })
-    }
+};
+
+function addInventory() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'ID',
+            message: "What is the product_id of the item you would like to update?"
+        },
+        {
+            type: 'input',
+            name: 'replenish',
+            message: 'How many units would you like to add to the inventory?'
+        }
+    ]).then(function (answers) {
+        connection.query('SELECT * FROM products WHERE item_id = ?', [answers.ID], function (error, results, fields) {
+            if (error) throw error;
+            var newQty = Math.floor(Number(results[0].stock_quantity) + Number(answers.replenish));
+            connection.query('UPDATE products SET stock_quantity = ? WHERE item_id = ?', [newQty, answers.ID], function (error, updateResults, fields) {
+                if (error) throw error;
+                console.log("Stock quantity:" + newQty)
+            })
+            start();
+        })
+    })
+};
+
+function addProduct(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'product',
+            message: "What's the name of your product?"
+        },
+        {
+            type: 'input',
+            name: 'department',
+            message: "What department does your product go in?"
+        },
+        {
+            type: 'input',
+            name: 'price',
+            message: "Price of your product?"
+        },
+        {
+            type: 'input',
+            name: 'quantity',
+            message: "How many units?"
+        },
+    ]).then(function(answers){
+        var post = {product_name: answers.product, department_name: answers.department, price: answers.price, stock_quantity: answers.quantity};
+        var query = connection.query('INSERT INTO products SET ?',post, function (error, results, fields) {
+            if (error) throw error;
+            console.log("product_name:"  + answers.product + "\n department_name:"  + answers.department + "\n price:" + answers.price + "\n stock_quantity:" + answers.quantity);
+            start();
+        })
+    })
+};
 
 
 
